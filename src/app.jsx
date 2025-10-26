308c4b4a5ec110a66608ac4c3e5fc0da8b10946b
Aayush-Ash <aayush.ash@gmail.com>
2025-10-26 19:04:38 +0530

    Resolve merge conflicts in index.html and index.css

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import gsap from "gsap";
-import { ScrollTrigger, SplitText } from "gsap/all";
-import Navbar from "./components/navbar.jsx";
-import Hero from "./components/hero.jsx";
-import Cocktails from "./components/public/Cocktails.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-gsap.registerPlugin(ScrollTrigger, SplitText)
+import {
+  GsapFrom,
+  GsapFromTo,
+  GsapScrollTrigger,
+  GsapStagger,
+  GsapText,
+  GsapTimeline,
+  GsapTo,
+  Home,
+} from "./pages";
+
 const App = () => {
-    return (
-        <main>
-            <Navbar/>
-            <Hero/>
-            <Cocktails />
+  return (
+    <div className="bg-black min-h-screen w-full">
+      <Router>
+        <Routes>
+          <Route path="/gsapto" element={<GsapTo />} />
+          <Route path="/gsapfrom" element={<GsapFrom />} />
+          <Route path="/gsapfromto" element={<GsapFromTo />} />
+          <Route path="/gsaptimeline" element={<GsapTimeline />} />
+          <Route path="/gsapstagger" element={<GsapStagger />} />
+          <Route path="/gsapscrolltrigger" element={<GsapScrollTrigger />} />
+          <Route path="/gsaptext" element={<GsapText />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
-        </main>
-    )
-}
-export default App
+export default App;
