894bd761ea627fd2b4d6c86e75fdc71959d00d9e
Aayush-Ash <aayush.ash@gmail.com>
2025-10-26 19:01:56 +0530

    Fix Vercel deployment issues: add Node.js version, fix GSAP imports, add vercel.json config

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import gsap from "gsap";
-import{ ScrollTrigger, SplitText } from "gsap/all";
+import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/navbar.jsx";
 import Hero from "./components/hero.jsx";
 import Cocktails from "./components/public/Cocktails.jsx";
