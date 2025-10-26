a6f6cb2cd7de955b9bd9e3690c605bc75b04adc2
Aayush-Ash <aayush.ash@gmail.com>
2025-10-26 19:06:55 +0530

    Fix Tailwind CSS configuration and build issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'modern-negra': ['Modern Negra', 'sans-serif'],
+        'serif': ['DM Serif Text', 'serif'],
+        'sans': ['Mona Sans', 'sans-serif'],
+      },
+      colors: {
+        'yellow': '#e7d393',
+        'white-100': '#efefef',
+      },
+    },
   },
   plugins: [],
-};
+}
